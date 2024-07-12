16a1263adc5f22f89863991b6675546044cae02c
MOUFAOUID <moufaouidmouhcine@gmail.com>
2024-07-12 23:11:51 +0100

    transaction fetch works

--- app/src/main/java/com/example/apiexample/MainActivity.kt
+++ app/src/main/java/com/example/apiexample/MainActivity.kt
@@ -1,14 +1,24 @@
 package com.example.apiexample
 
-import android.content.Context
 import android.os.Bundle
 import android.util.Log
-import android.view.View
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
@@ -20,13 +30,15 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.apiexample.api.UserApi
 import com.example.apiexample.ui.theme.ApiExampleTheme
 import com.example.apiexample.ui.theme.Purple700
-import retrofit2.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : ComponentActivity() {
@@ -47,9 +59,9 @@ class MainActivity : ComponentActivity() {
 }
 
 data class ProfileModel(
-    var age: String,
     var name: String,
-    var email: String,
+    var password: String
+
 )
 
 data class UserModel(
@@ -58,101 +70,100 @@ data class UserModel(
 
 @Composable
 fun MainScreen() {
-   Scaffold(
-       topBar = {
-           TopAppBar(
-               backgroundColor = Purple700,
-               title = {
-                   Text(
-                       text = "Simple API Request",
-                       modifier = Modifier.fillMaxWidth(),
-                       textAlign = TextAlign.Center,
-                       color = Color.White
-                   )
-               }
-           )
-       },
-       content = {
-          Column(
-              modifier = Modifier.fillMaxWidth(),
-              verticalArrangement = Arrangement.Center,
-              horizontalAlignment = Alignment.CenterHorizontally
-          ) {
-              val id = remember {
-                  mutableStateOf(TextFieldValue())
-              }
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                backgroundColor = Purple700,
+                title = {
+                    Text(
+                        text = "Simple API Request",
+                        modifier = Modifier.fillMaxWidth(),
+                        textAlign = TextAlign.Center,
+                        color = Color.White
+                    )
+                }
+            )
+        },
+        content = {
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                val id = remember {
+                    mutableStateOf(TextFieldValue())
+                }
 
-              val profile = remember {
-                  mutableStateOf(ProfileModel(
-                      age = "",
-                      name = "",
-                      email = ""
-                  ))
-              }
+                val transaction = remember {
+                    mutableStateOf(
+                        TransactionDTO()
+                    )
+                }
 
-              Text(
-                  text="API Sample",
-                  style= TextStyle(
-                      fontSize = 40.sp,
-                      fontFamily = FontFamily.Cursive
-                  )
-              )
+                Text(
+                    text = "API Sample",
+                    style = TextStyle(
+                        fontSize = 40.sp,
+                        fontFamily = FontFamily.Cursive
+                    )
+                )
 
-              Spacer(modifier = Modifier.height(15.dp))
+                Spacer(modifier = Modifier.height(15.dp))
 
-              TextField(
-                  label = { Text(text = "User ID")},
-                  value = id.value,
-                  onValueChange = { id.value = it }
-              )
+                TextField(
+                    label = { Text(text = "User ID") },
+                    value = id.value,
+                    onValueChange = { id.value = it }
+                )
 
-              Spacer(modifier = Modifier.height(15.dp))
+                Spacer(modifier = Modifier.height(15.dp))
 
-              Box(modifier = Modifier.padding(40.dp, 0.dp, 40.dp, 0.dp)) {
-                  Button(
-                      onClick = {
-                          val data = sendRequest(
-                              id = id.value.text,
-                              profileState = profile
-                          )
+                Box(modifier = Modifier.padding(40.dp, 0.dp, 40.dp, 0.dp)) {
+                    Button(
+                        onClick = {
+                            val data = sendRequest(
+                                id = id.value.text,
+                                transaction = transaction
+                            )
 
-                          Log.d("Main Activity", profile.toString())
-                      }
-                  ) {
-                      Text(text = "Get Data")
-                  }
-              }
-              
-              Spacer(modifier = Modifier.height(15.dp))
-              
-              Text(text = profile.component1().toString(), fontSize = 40.sp)
-          }
-       }
-   )
+                            Log.d("Main Activity", transaction.toString())
+                        }
+                    ) {
+                        Text(text = "Get Data")
+                    }
+                }
+
+                Spacer(modifier = Modifier.height(15.dp))
+
+                Text(text = transaction.component1().toString(), fontSize = 40.sp)
+            }
+        }
+    )
 }
 
 fun sendRequest(
     id: String,
-    profileState: MutableState<ProfileModel>
+    transaction: MutableState<TransactionDTO>
 ) {
     val retrofit = Retrofit.Builder()
-        .baseUrl("http://192.168.0.109:3000")
+        .baseUrl("http://192.168.1.83:9000/")
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
     val api = retrofit.create(UserApi::class.java)
 
-    val call: Call<UserModel?>? = api.getUserById(id);
+    val call: Call<TransactionResponse?>? = api.getUsers();
 
-    call!!.enqueue(object: Callback<UserModel?> {
-        override fun onResponse(call: Call<UserModel?>, response: Response<UserModel?>) {
-            if(response.isSuccessful) {
+    call!!.enqueue(object : Callback<TransactionResponse?> {
+
+        override fun onResponse(call: Call<TransactionResponse?>, response: Response<TransactionResponse?>) {
+            if (response.isSuccessful) {
                 Log.d("Main", "success!" + response.body().toString())
-                profileState.value = response.body()!!.profile
+                transaction.value = response.body()!!.transactionDTOS.content.get(0);
             }
         }
 
-        override fun onFailure(call: Call<UserModel?>, t: Throwable) {
+        override fun onFailure(call: Call<TransactionResponse?>, t: Throwable) {
             Log.e("Main", "Failed mate " + t.message.toString())
         }
     })
--- app/src/main/java/com/example/apiexample/api/UserApi.kt
+++ app/src/main/java/com/example/apiexample/api/UserApi.kt
@@ -1,6 +1,8 @@
 package com.example.apiexample.api
 
 import com.example.apiexample.ProfileModel
+import com.example.apiexample.TransactionDTO
+import com.example.apiexample.TransactionResponse
 import com.example.apiexample.UserModel
 import okhttp3.ResponseBody
 import retrofit2.Call
@@ -17,4 +19,9 @@ public interface UserApi {
     )
     @GET("users/{id}")
     abstract fun getUserById(@Path("id") id: String): Call<UserModel?>?
+//    @GET("api/front-office/transactionId-search?status=Null&perPage=5&page=1&merchantId=20003&acquirer&issuer&date&transactionId&transactionType")
+//    abstract fun getUsers(): Call<TransactionResponse?>?
+
+    @GET("transactions")
+    abstract fun getUsers(): Call<TransactionResponse?>?
 }
